361757b21ddfd9423f9edcde38588749db4e0867
tsuji <s173058@matsu.shimane-u.ac.jp>
2019-11-10 00:44:46 +0900

    どう？

--- last/Assets/Scripts/Test.cs
+++ last/Assets/Scripts/Test.cs
@@ -14,8 +14,8 @@ public class Test : MonoBehaviour
     public static string SampleStart; //入力するやつ　staticをつけた（長田）
     public static string SampleGoal;　//staticをつけた（長田）
 
-    int start;
-    int goal;
+    int start = 99;
+    int goal = 99;
 
     public static int ekisuu; //合計値　staticをつけた(長田)
 
@@ -49,34 +49,41 @@ public class Test : MonoBehaviour
     {
         es = Resources.Load("takeuchi") as Entity_Sheet1; //エクセル呼び出し  
         int i;
-        for (i = 0; i < 20; i++)
-        {//長さはlength取得に変更すべき
-            if (SampleStart.Equals(es.sheets[0].list[i].name))
-            {
-                Debug.Log("発:" + es.sheets[0].list[i].name);//発：ありましたのデバッグ表示
-                start = i;//発の駅の番号を保管しておく
-            }
-            if (SampleGoal.Equals(es.sheets[0].list[i].name))
-            {
-                Debug.Log("着:" + es.sheets[0].list[i].name);//着：ありましたのデバッグ表示
-                goal = i;//着の駅の番号を保管しておく
-            }
-        }
-        if (goal > start)
+        if (SampleStart != null && SampleGoal != null)
         {
-            ekisuu = goal - start; //駅数を算出
+            for (i = 0; i < 20; i++)
+            {//長さはlength取得に変更すべき
+                if (SampleStart.Equals(es.sheets[0].list[i].name))
+                {
+                    Debug.Log("発:" + es.sheets[0].list[i].name);//発：ありましたのデバッグ表示
+                    start = i;//発の駅の番号を保管しておく
+                }
+                if (SampleGoal.Equals(es.sheets[0].list[i].name))
+                {
+                    Debug.Log("着:" + es.sheets[0].list[i].name);//着：ありましたのデバッグ表示
+                    goal = i;//着の駅の番号を保管しておく
+                }
+            }
         }
-        else
+
+        if (goal != 99 && start != 99)
         {
-            ekisuu = start - goal; //駅数を算出
+            if (goal > start)
+            {
+                ekisuu = goal - start; //駅数を算出
+            }
+            else
+            {
+                ekisuu = start - goal; //駅数を算出
+            }
+            /* num.text = ekisuu.ToString(); int型（数字のみ）からstring型（文字）へ*/ //バグが出るので隠してる（長田）
+                                                                        /*st.text = es.sheets[0].list[start].name;*/ //いらんかも（長田）
+            Debug.Log("駅数:" + ekisuu);
+            /*go.text = es.sheets[0].list[goal].name;*/ //いらんかも（長田）
+
+            //もし駅がなかったらの処理が必要  
+
+            SceneManager.LoadScene("ResultScene"); //次のシーンに飛ぶ。次のシーンの名前を書く（長田）
         }
-       /* num.text = ekisuu.ToString(); int型（数字のみ）からstring型（文字）へ*/ //バグが出るので隠してる（長田）
-        /*st.text = es.sheets[0].list[start].name;*/ //いらんかも（長田）
-        Debug.Log("駅数:" + ekisuu);
-        /*go.text = es.sheets[0].list[goal].name;*/　//いらんかも（長田）
-
-        //もし駅がなかったらの処理が必要  
-
-        SceneManager.LoadScene("ResultScene");　//次のシーンに飛ぶ。次のシーンの名前を書く（長田）
     }
 }
